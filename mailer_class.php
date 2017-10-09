54c763781770a19e36a4bc7c23f6e93626646d97
jacobkri <jacobkweb@gmail.com>
2017-10-09 09:34:58 +0200

    Started implementing db and fh objects

--- mailer_class.php
+++ mailer_class.php
@@ -4,21 +4,32 @@
 
 
 class mailer_class {
-	
-  public function __construct() {
-	  
+  private $charset;
+  private $db = false; // Database object
+  private $fh = false; // File_handler object
+  
+  public function __construct($db=false, $fh=false) {
+	if ($db != false) {
+	  // If a database object was injected, rely on database for list subscribtions
+	  $this->db = $db;
+	}
+	if ($fh != false) {
+	  // Use external file handler object if available, otherwise use basic file handling
+	  $this->fh = $fh;
+	}
+	// Settings
+    $this->charset = 'UTF-8';
   }
   public function send_email() {
 	$to = 'johny@example.com, sally@example.com';
 
 	$subject = '';
+	$message = ''; // text/html source
 
-	$message = '';
-
+    $headers = array();	
 	$headers[] = 'MIME-Version: 1.0';
-	$headers[] = 'Content-type: text/html; charset=UTF-8';
-
-	$headers[] = 'To: ';
+	$headers[] = 'Content-type: text/html; charset=' . $this->charset;
+	$headers[] = 'To: '; // Comma seperated, same order as in recipients ($to)
 	$headers[] = 'From: ';
 	// $headers[] = 'Cc: ';
 	// $headers[] = 'Bcc: ';
@@ -26,7 +37,7 @@ class mailer_class {
 	mail($to, $subject, $message, implode("\r\n", $headers));
   }
   public function list_subscribe($list_id) {
-	  
+
   }
   public function list_unsubscribe($list_id) {
 	  
